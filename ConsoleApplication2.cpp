b4d159342ee7d0086a5f578a83aac9ce06c388c7
12138 <48377190+wqreytuk@users.noreply.github.com>
2023-09-13 10:42:23 +0800

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿
+
+
+#include <stdio.h>
+#include <windows.h>
+
+
+int main() {
+    HANDLE _file_handle = CreateFileA("C:\\users\\lc\\1.txt",
+        GENERIC_ALL,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+
+    HANDLE _file_map = CreateFileMappingA(_file_handle,
+        NULL,
+        PAGE_EXECUTE_READWRITE,
+        0,
+        0,
+        NULL);
+
+    LPVOID _view_address = MapViewOfFile(_file_map,
+        FILE_MAP_ALL_ACCESS,
+        0,
+        0,
+        0);
+
+    printf("%s\n", (char*)_view_address);
+    printf("--------------------\n\n");
+    *(char*)_view_address = '0';
+
+    printf("%s\n", (char*)_view_address);
+}
